bebc8181cd617d7943189e0c8e9526e5b506c909
K4alawad <kalawad@bessermittag.de>
2024-04-03 16:37:13 +0200

    odoo config

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+    'category': 'Tutorials/estate',
+    'version': '0.1',
+    'summary': '',
+    'description': "",
+    'depends': ['base'],
+    'application': True,
+}
