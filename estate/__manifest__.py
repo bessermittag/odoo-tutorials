fdcdfe317b35587cf3169916b801cd6e3b28b0d7
Christoph Fischer <cfischer@nextorder.de>
2024-04-05 09:58:25 +0200

    Chapter 8: Relations Between Models

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,7 +9,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_menus.xml',
         ],
     'application': True,
     'license': 'LGPL-3',
