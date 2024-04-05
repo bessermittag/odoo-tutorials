5f748faaf1bcce94e9de0e62a7c750ff52ac486d
Christoph Fischer <cfischer@nextorder.de>
2024-04-05 16:16:10 +0200

    Replacing double quotations with single quotations in estate module, fix typos, adjust some write logic

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Real Estate",
+    'name': 'Real Estate',
     'category': 'Tutorials/estate',
     'version': '0.1',
     'summary': '',
-    'description': "",
-    'depends': ['base','mail'],
+    'description': '',
+    'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
-        'views/res_users_views.xml',
         ],
     'application': True,
     'license': 'LGPL-3',
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -3,8 +3,8 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 class PropertyOfferModel(models.Model):
-    _name = "estate.property.offer"
-    _description = "Estate Property Offer Model"
+    _name = 'estate.property.offer'
+    _description = 'Estate Property Offer Model'
 
     price = fields.Float()
     status = fields.Selection(selection=[('accepted','Accepted'),('refused','Refused')],copy=False)
@@ -13,40 +13,42 @@ class PropertyOfferModel(models.Model):
     validity = fields.Integer(string='Validity (days)',default=7)
     date_deadline = fields.Date(string='Deadline',compute='_compute_date_deadline',inverse='_inverse_date_deadline')
 
-    @api.depends("validity","date_deadline")
+    @api.depends('validity','date_deadline')
     def _compute_date_deadline(self):
         for record in self:
             create_date = record.create_date if record.create_date else fields.Datetime.today()
             record.date_deadline = fields.Date.add(create_date.date(), day=record.validity)
 
-    @api.depends("validity","date_deadline")
+    @api.depends('validity','date_deadline')
     def _inverse_date_deadline(self):
         for record in self:
-            create_date = record.create_date if record.create_date else fields.Datefield.today()
+            create_date = record.create_date if record.create_date else fields.Datetime.today()
             record.validity = (record.date_deadline - create_date.date()).days
 
     def action_accept_offer(self):
         for record in self:
             if record.property_id.state in ['sold','canceled']:
-                raise UserError(_("Offers for canceled or sold Properties can't be accepted."))
+                raise UserError(_('Offers for canceled or sold Properties can\'t be accepted.'))
             elif record.property_id.state == 'offer_accepted':
-                raise UserError(_("There is already an accepted Offer for this Property."))
+                raise UserError(_('There is already an accepted Offer for this Property.'))
             record.status = 'accepted'
-            record.property_id.state = 'offer_accepted'
-            record.property_id.partner_id = record.partner_id
-            record.property_id.selling_price = record.price
-            for other_offer in record.property_id.offer_ids:
-                if other_offer.id != record.id:
-                    other_offer.status = 'refused'
+            record.property_id.write({
+                'state': 'offer_accepted',
+                'partner_id': record.partner_id,
+                'selling_price': record.price,
+            })
+            (record.property_id.offer_ids - record).status = 'refused'
         return True
 
     def action_refuse_offer(self):
         for record in self:
             if record.property_id.state in ['sold','canceled']:
-                raise UserError(_("Offers for canceled or sold Properties can't be refused."))
-            if record.status == 'accpted':
-                record.property_id.partner_id = False
-                record.property_id.selling_price = False
-                record.property_id.state = 'new'
+                raise UserError(_('Offers for canceled or sold Properties can\'t be refused.'))
+            if record.status == 'accepted':
+                record.property_id.write({
+                    'partner_id': False,
+                    'selling_price': False,
+                    'state': 'new',
+                })
             record.status = 'refused'
         return True
