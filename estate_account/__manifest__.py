9a9cae55f5619e69f0ddd83e3213b30321cedbd6
wilpe <peter.wilke03@gmail.com>
2024-04-12 09:47:39 +0200

    exercise 14 Interact with other Modules

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate Account',
+    'category': 'Tutorials/estate',
+    'version': '0.1',
+    'summary': 'Accounting and Invoicing features for the Real Estate Model',
+    'depends': ['estate', 'account'],
+    'data': [],
+    'application': True,
+    'license': 'LGPL-3'
+}
+
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api , _
+
+class EstatePropertyInherit(models.Model):
+    _inherit = 'estate.property'
+
+    def action_sold(self):
+      self.env['account.move'].create({
+          'partner_id': self.partner_id.id,
+          'move_type': 'out_invoice',
+          'invoice_date': fields.Date.today(),
+          'invoice_line_ids': [(0, 0, {
+                'name': 'Property Sale',
+                'quantity': 1,
+                'price_unit': self.selling_price * 0.06,
+            }), (0, 0, {
+                'name': 'Administrative Fees',
+                'quantity': 1,
+                'price_unit': 100.00,
+            })]
+        })
+      return super(EstatePropertyInherit, self).action_sold()
