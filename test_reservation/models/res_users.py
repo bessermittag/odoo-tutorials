5e047c78cd8155ab6d4e28e51c79a934534b869f
wilpe <peter.wilke03@gmail.com>
2024-04-17 14:21:44 +0200

    use case test_reservation

--- test_reservation/models/res_users.py
+++ test_reservation/models/res_users.py
@@ -1,4 +1,10 @@
-from odoo import api, fields, models, Command
+from odoo import api, fields, models, Command,exceptions
+from datetime import timedelta
+import binascii
+import os
+
+API_KEY_SIZE = 20
+INDEX_SIZE = 8
 
 
 class ResUsers(models.Model):
@@ -11,6 +17,8 @@ class ResUsers(models.Model):
     _sql_constraints = [
         ('main_order_id_uniq', 'unique(main_order_id)', 'A user\'s main order is his own.'),
     ]
+
+    api_key_expiration_date = fields.Datetime(string='API Key Expiration Date')
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -35,3 +43,51 @@ class ResUsers(models.Model):
             self._create_main_order()
         self.main_order_id._clear_old_reservations()
         return self.main_order_id.order_line.filtered(lambda sol: not sol.display_type and sol.product_uom_qty > 0)
+
+
+
+class APIKeys(models.Model):
+    _inherit = 'res.users.apikeys'
+
+    expiration_date = fields.Datetime(string='Expiration Date')
+    #TODO: Add functionality to set expiration date when creating API key
+    #TODO: Add functionalty to expand expiration date for a user
+    #TODO: Add functionality to check expiration date for a user
+    #TODO: set default expiration date to 30 days
+
+
+    def _generate(self, scope, name, expiration_date=None):
+        k = binascii.hexlify(os.urandom(API_KEY_SIZE)).decode()
+        # store the ex_date in the database
+        self.env.cr.execute("""
+        INSERT INTO {table} (name, user_id, scope, key, index, expiration_date)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        RETURNING id
+        """.format(table=self._table),
+        [name, self.env.user.id, scope, KEY_CRYPT_CONTEXT.hash(k), k[:INDEX_SIZE], expiration_date])
+
+        ip = request.httprequest.environ['REMOTE_ADDR'] if request else 'n/a'
+        _logger.info("%s generated: scope: <%s> for '%s' (#%s) from %s",
+            self._description, scope, self.env.user.login, self.env.uid, ip)
+
+        return k
+
+    def _check_credentials(self, *, scope, key):
+
+        assert scope, "scope is required"
+        index = key[:INDEX_SIZE]
+        self.env.cr.execute('''
+               SELECT user_id, key, expiration_date
+               FROM {} INNER JOIN res_users u ON (u.id = user_id)
+               WHERE u.active and index = %s AND (scope IS NULL OR scope = %s) AND (expiration_date IS NULL OR expiration_date > now())
+           '''.format(self._table),
+                            [index, scope])
+        for user_id, current_key, expiration_date in self.env.cr.fetchall():
+            if KEY_CRYPT_CONTEXT.verify(key, current_key):
+                # check expiration date
+                if expiration_date and fields.Datetime.from_string(expiration_date) < datetime.now():
+                    raise exceptions.UserError('API Key expired')
+                return super()._check_credentials(scope=scope, key=key)
+
+#add a new scope for a new type of user( e.g scope =4 user-specific) and specify the expiration date for type of the scope
+
